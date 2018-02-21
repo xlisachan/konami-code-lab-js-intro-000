5337ed0793869d002fdd43ade4a0237c10afdd0d
Lisa Chan <xlisachan@gmail.com>
2018-02-21 01:13:15 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,17 +3,19 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 let index = 0;
 
 function init() {
+  document.body.addEventListener('keydown', function (e) {
   const key = parseInt(e.detail || e.which);
-
+  
   if (key === code[index]) {
     index++;
 
     if (index === code.length) {
-      alert("Hurray!");
+      alert("Congratulations!");
 
       index = 0;
     }
   } else {
     index = 0;
   }
+  });
 }
