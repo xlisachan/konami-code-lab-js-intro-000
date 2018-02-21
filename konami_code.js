8af84659c7a93b638c5fc2540e6d130f87483021
Lisa Chan <xlisachan@gmail.com>
2018-02-21 01:13:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,7 +5,7 @@ let index = 0;
 function init() {
   document.body.addEventListener('keydown', function (e) {
   const key = parseInt(e.detail || e.which);
-  
+
   if (key === code[index]) {
     index++;
 
